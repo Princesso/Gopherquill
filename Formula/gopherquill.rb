487217befe2a763ef932735c26bfb54ae7802175
Oluebube Princes Egbuna <egbunaoluebube@gmail.com>
2024-11-24 10:52:25 +0100

    Update hombrew formula

---
+++ Formula/gopherquill.rb
@@ -0,0 +1,17 @@
+class Gopherquill < Formula
+    desc "A powerful code review tool for developers"
+    homepage "https://github.com/Princesso/gopherquill"
+    url "https://github.com/Princesso/gopherquill/archive/refs/tags/v0.1.1.tar.gz"
+    sha256 "0a6c8831d314ce8bc079f3ce03e53674fcf11778"
+  
+    depends_on "go" => :build
+  
+    def install
+      system "go", "build", "-o", bin/"gopherquill"
+    end
+  
+    test do
+      system "#{bin}/gopherquill", "--help"
+    end
+  end
+  
